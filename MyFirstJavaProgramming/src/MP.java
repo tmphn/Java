72a649138815058c1134dd5f88bf45d390f574f9
tmphn <54253432+tmphn@users.noreply.github.com>
2019-10-05 22:44:10 -0700

    array

--- MyFirstJavaProgramming/src/MP.java
+++ MyFirstJavaProgramming/src/MP.java
@@ -4,17 +4,33 @@ public class MP {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		
+		System.out.println("Give a size: "); 
 		Scanner in = new Scanner(System.in);
-		String s = in.nextLine();
+		int size = in.nextInt();
+		
+		
+		int[] grades = new int[size];
+		//int scores[];
+		System.out.println("Enter " + size + " numbers. Press enter after each");
+		for(int i = 0; i < size; i++) {
+			grades[i] = in.nextInt();
+		
+		}
+		in.close();
+		
+		for(int i = 0; i < size; i++) {
+			System.out.println(grades[i]);
+		}
+		
+		/*String s = in.nextLine();
 		
 		if(s.equals("Tam")) {
 			System.out.println("Hey, Tam");
 		} else if (s.equals("Anon")) {
 			System.out.println("Hey, Anon");
-		}
+		}*/
 		
-		in.close();
+		
 		//while loop
 		/*int i = 0;
 		while(i < 10) {
@@ -23,16 +39,17 @@ public class MP {
 		}*/
 		
 		//do while loop
-		/*do {
+		/*int i = 0;
+		  do {
 			System.out.println("i: " + i);
 		} while(i < 10);*/
 		
 		//for loop
-		for(int i = 0; i< 10; i++) {
+		/*for(int i = 0; i< 10; i++) {
 			System.out.println(i);
-		}
+		}*/
 			
-		System.out.println(s);
+		//System.out.println(s);
 	}
 
 }
